4349f4a5ecf130ff5544239571674815034c4c90
WAM <eraserw@gmail.com>
2022-11-16 14:13:34 +0100

    essai1js-arnaud

--- script.js
+++ script.js
@@ -5,8 +5,23 @@ const player = document.getElementById('player');
 const ia = document.getElementById('ia');
 const command = document.getElementById('command');
 
-shi.addEventListener('click', () => console.log('shi'));
+shi.addEventListener('click', printShi );
 fu.addEventListener('click', () => console.log('fu'));
 mi.addEventListener('click', () => console.log('mi'));
 
 // Pour le reste, a vous de jouer
+
+function printShi (){
+    var img = document.createElement('img');
+    img.src ="./img/shi.png";
+
+    player.setAttribute("url",img);
+
+
+
+
+    console.log("Gagné");
+
+    var hidden = document.getElementsByClassName('hidden');
+    hidden.setAttribute("style","display : initial");
+};
