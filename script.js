77e224480a66a6afd001c6a60b5e235408a0dc22
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-16 15:29:17 +0100

    fonction random ia added

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ mi.addEventListener('click', () => console.log('mi'));
 
 // Pour le reste, a vous de jouer
 
-shi.addEventListener('click', () => { player.setAttribute("src", "/img/shi.png");});
+const IA_CHOICE = ["shi", "fu", "mi"];
+
+shi.addEventListener('click', () => { player.setAttribute("src", "/img/shi.png");
+    let rand = Math.floor(Math.random()*IA_CHOICE.length);
+    let randIa = IA_CHOICE[rand];
+    ia.setAttribute("src", `/img/${randIa}.png`);
+});
+
+
 fu.addEventListener('click', () => { player.setAttribute("src", "/img/fu.png");});
+
+
 mi.addEventListener('click', () => { player.setAttribute("src", "/img/mi.png");});
+
+
+
